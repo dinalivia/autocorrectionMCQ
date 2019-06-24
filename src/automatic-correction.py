2691fe2293fcd716bd8a0b632677f8e9aa36a02c
dinalivia <dinalivia@bct.ect.ufrn.br>
2019-06-23 17:47:53 -0700

    Sistem GUI

--- src/automatic-correction.py
+++ src/automatic-correction.py
@@ -23,88 +23,54 @@ import Tkinter, Tkconstants, tkFileDialog
 #import process_gabarito
 #import correction
 
-'''
-main = Tk()
-
-main.title("Correcao automatica")
-main.geometry("500x400")
-var = StringVar()
-label = Label(main, textvariable=var, relief=RAISED)
-
-var.set("Selecione arquivo do gabarito")
-label.pack()
-
-doneButton = Tkinter.Button(main, text="botao", width=25, command=main.destroy)
-doneButton.pack()
-
-main.mainloop()
-
-# receive path to read images
-sec = Tk()
-#root.filename = tkFileDialog.asksaveasfilename(initialdir = "/home/pi", title = "Selecione o arquivo de gabarito", filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-
-sec.filename = tkFileDialog.askopenfilename(initialdir = "/home/pi/git/TCC/autocorrectionMCQ", title = "Selecione o arquivo de gabarito")
-print (sec.filename)
-
-sec.directory = tkFileDialog.askdirectory()
-print (sec.directory)
-'''
-
-
 form = Tkinter.Tk()
 
 getFld = Tkinter.IntVar()
 
 form.wm_title('Correcao Automatica de provas multipla escolha')
 
-# -- Gabarito --
+
+def searchkeyfile(_title):
+    sec = Tk()
+    sec.filename = tkFileDialog.askopenfilename(initialdir = "/home/pi/git/TCC/autocorrectionMCQ", title = _title)
+    print("passou aqui")
+    sec = 
+    return sec.filenamme
+    
+
+
+
+# ----- Gabarito -----
+
+# Titulo
 stepOne = Tkinter.LabelFrame(form, text=" 1. Informe o gabarito da prova: ")
 stepOne.grid(row=0, columnspan=7, sticky='W', \
              padx=5, pady=5, ipadx=5, ipady=5)
-
+#checkbox
 transChk = Tkinter.Checkbutton(stepOne, \
            text="Escrever sequencia", onvalue=1, offvalue=0)
 transChk.grid(row=0, column=0, sticky='WE', padx=3, pady=2)
-
 transRwLbl = Tkinter.Label(stepOne, \
                            text=" => Ex. a,b,c,d,e,a,b,c, ...")
 transRwLbl.grid(row=0, column=1, columnspan=2, \
                 sticky='W', padx=5, pady=2)
-
+# Entrada gabarito
 transRwTxt = Tkinter.Entry(stepOne)
 transRwTxt.grid(row=1, columnspan=5, sticky='WE', padx=5, pady=2)
 
+# Buscar gabarito
 inFileLbl = Tkinter.Label(stepOne, text="Escolher imagem do gabarito:")
 inFileLbl.grid(row=2, column=0, sticky='W ', padx=5, pady=2)
 
 inFileTxt = Tkinter.Entry(stepOne)
 inFileTxt.grid(row=2, column=1, columnspan=7, sticky="WE",pady=3)
 
-inFileBtn = Tkinter.Button(stepOne, text="Buscar")
+inFileBtn = Tkinter.Button(stepOne, text="Buscar", command = searchkeyfile("Selecione o arquivo de gabarito"))
 inFileBtn.grid(row=2, column=8, sticky='WE', padx=5, pady=2)
 
-#outFileLbl = Tkinter.Label(stepOne, text="Save File to:")
-#outFileLbl.grid(row=2, column=0, sticky='E', padx=5, pady=2)
 
-#outFileTxt = Tkinter.Entry(stepOne)
-#outFileTxt.grid(row=2, column=1, columnspan=7, sticky="WE", pady=2)
+# ------- Respostas dos docentes -------
 
-#outFileBtn = Tkinter.Button(stepOne, text="Browse ...")
-#outFileBtn.grid(row=2, column=8, sticky='W', padx=5, pady=2)
-
-#inEncLbl = Tkinter.Label(stepOne, text="Input File Encoding:")
-#inEncLbl.grid(row=3, column=0, sticky='E', padx=5, pady=2)
-
-#inEncTxt = Tkinter.Entry(stepOne)
-#inEncTxt.grid(row=3, column=1, sticky='E', pady=2)
-
-#outEncLbl = Tkinter.Label(stepOne, text="Output File Encoding:")
-#outEncLbl.grid(row=3, column=5, padx=5, pady=2)
-
-#outEncTxt = Tkinter.Entry(stepOne)
-#outEncTxt.grid(row=3, column=7, pady=2)
-
-# -- Respostas dos docentes --
 stepTwo = Tkinter.LabelFrame(form, text=" 2. Informe as respostas dos docentes: ")
 stepTwo.grid(row=3, columnspan=7, sticky='W', \
              padx=5, pady=5, ipadx=5, ipady=5)
@@ -118,27 +84,9 @@ inFileTxt2.grid(row=4, column=1, columnspan=7, sticky="WE", pady=3)
 inFileBtn2 = Tkinter.Button(stepTwo, text="Buscar")
 inFileBtn2.grid(row=4, column=8, sticky='W', padx=5, pady=2)
 
-#outTblLbl = Tkinter.Label(stepTwo, \
-#      text="Enter the name of the table to be used in the statements:")
-#outTblLbl.grid(row=3, column=0, sticky='W', padx=5, pady=2)
 
-#outTblTxt = Tkinter.Entry(stepTwo)
-#outTblTxt.grid(row=3, column=1, columnspan=3, pady=2, sticky='WE')
+# ------- Processando e salvando resultados -------
 
-#fldLbl = Tkinter.Label(stepTwo, \
-#                       text="Enter the field (column) names of the table:")
-#fldLbl.grid(row=5, column=0, padx=5, pady=2, sticky='W')
-
-#getFldChk = Tkinter.Checkbutton(stepTwo, \
-#                       text="Get fields automatically from input file",\
-#                       onvalue=1, offvalue=0)
-#getFldChk.grid(row=5, column=1, columnspan=3, pady=2, sticky='WE')
-
-#fldRowTxt = Tkinter.Entry(stepTwo)
-#fldRowTxt.grid(row=6, columnspan=5, padx=5, pady=2, sticky='WE')
-
-
-# -- Processando e salvando resultados --
 stepThree = Tkinter.LabelFrame(form, text=" 3. Resultado das correcoes ")
 stepThree.grid(row=5, columnspan=7, sticky='W', \
                padx=5, pady=5, ipadx=5, ipady=5)
@@ -152,29 +100,30 @@ inFileTxt3.grid(row=4, column=1, columnspan=7, sticky="WE", pady=3)
 inFileBtn3 = Tkinter.Button(stepThree, text="Buscar")
 inFileBtn3.grid(row=4, column=8, sticky='W', padx=5, pady=2)
 
-'''
-transChk = Tkinter.Checkbutton(stepThree, \
-           text="Enable Transaction", onvalue=1, offvalue=0)
-transChk.grid(row=6, sticky='W', padx=5, pady=2)
 
-transRwLbl = Tkinter.Label(stepThree, \
-             text=" => Specify number of rows per transaction:")
-transRwLbl.grid(row=6, column=2, columnspan=2, \
-                sticky='W', padx=5, pady=2)
+# ------- Help section -------
 
-transRwTxt = Tkinter.Entry(stepThree)
-transRwTxt.grid(row=6, column=4, sticky='WE')
-'''
-
-
-# -- Help section --
 helpLf = Tkinter.LabelFrame(form, text=" Ajuda ")
 helpLf.grid(row=0, column=9, columnspan=2, rowspan=8, \
             sticky='WE', \
              padx=5, pady=5, ipadx=5, ipady=5)
-helpLbl = Tkinter.Label(helpLf, text="Este e o programa de correcao automatica de provas de multipla escolha. Para efetuar as correcoes, preencha os campos  ao lado. \n Passo 1. Definir sequencia de respostas corretas do gabarito ou informar imagem do gabarito correto preenchido \n Passo 2. Informar diretorio com imagens de respostas dos alunos. \n Passo 3. Escolher diretorio para salvar o resultado dos alunos")
-helpLbl.grid(row=0)
 
+helpText = Tkinter.Text(helpLf,width = 30, height=15)
+
+scrollbar = Scrollbar(form, command=helpText.yview, orient=VERTICAL)
+scrollbar.grid(row=0, column=9, columnspan=2, rowspan=8, \
+            sticky='NES', \
+             padx=5, pady=5, ipadx=5, ipady=5)
+
+scrollbar.config(command=helpText.yview)
+
+helpText.insert(INSERT, "\nEste e o programa de correcao automatica de provas de multipla escolha. Para efetuar as correcoes, preencha os campos  ao lado. \n\n -> Passo 1. \nDefinir sequencia de respostas corretas do gabarito ou informar imagem do gabarito correto preenchido \n\n -> Passo 2. \nInformar diretorio com imagens de respostas dos alunos. \n\n -> Passo 3. \nEscolher diretorio para salvar o resultado dos alunos")
+
+helpText.configure(yscrollcommand=scrollbar.set, state='disable')
+
+helpText.grid(row=0, column=0, columnspan=2, rowspan=8, \
+            sticky='W', \
+             padx=5, pady=5, ipadx=5, ipady=5)
 
 form.mainloop()
 
@@ -186,4 +135,20 @@ form.mainloop()
 
 #pre-processing images
 
+
+
+'''
+
+
+main.mainloop()
+
+# receive path to read images
+sec = Tk()
+#root.filename = tkFileDialog.asksaveasfilename(initialdir = "/home/pi", title = "Selecione o arquivo de gabarito", filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+
+
+
+sec.directory = tkFileDialog.askdirectory()
+print (sec.directory)
+'''
 
