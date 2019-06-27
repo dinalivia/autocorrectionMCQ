3c2738af19623918a22d8364274a7417b280d3f5
dinalivia <dinalivia@bct.ect.ufrn.br>
2019-06-26 17:53:51 -0700

    add askdialog

--- src/automatic-correction.py
+++ src/automatic-correction.py
@@ -27,21 +27,30 @@ import Tkinter, Tkconstants, tkFileDialog
 #import process_gabarito
 #import correction
 
+from tkFileDialog   import askopenfilename, askdirectory
+
+gab_img = ''
+ans_dir = ''
+res_dir = ''
+
+def open_gab():
+    gab_img = askopenfilename()
+    inFileTxt.insert(INSERT,gab_img)
+
+
+def open_dir1():
+    ans_dir = askdirectory()
+    inFileTxt2.insert(INSERT,ans_dir)
+
+def open_dir2():
+    res_dir = askdirectory()
+    inFileTxt3.insert(INSERT,res_dir)
+    
 form = Tkinter.Tk()
 getFld = Tkinter.IntVar()
-gab_img = ''
 
 form.wm_title('Correção Automática de provas múltipla escolha')
 
-
-
-def searchkeyfile(_title):
-   filename = tkFileDialog.askopenfilename(initialdir =
-                                                "/home/pi/git/TCC/autocorrectionMCQ",
-                                                title = _title)
-   return filename
-
-    
 # ----- Gabarito -----
 
 # Titulo
@@ -67,12 +76,12 @@ transRwTxt.grid(row=1, columnspan=5, sticky='WE', padx=5, pady=2)
 inFileLbl = Tkinter.Label(stepOne, text="Escolher imagem do gabarito:")
 inFileLbl.grid(row=2, column=0, sticky='W ', padx=5, pady=2)
 
-inFileTxt = Tkinter.Entry(stepOne)
+inFileTxt = Tkinter.Text(stepOne,width = 10, height=1)
 inFileTxt.grid(row=2, column=1, columnspan=7, sticky="WE",pady=3)
 
-inFileBtn = Tkinter.Button(stepOne, text="Buscar", command = searchkeyfile
-                           ("Selecione o arquivo de gabarito"))
+inFileBtn = Tkinter.Button(stepOne, text="Buscar", command = open_gab)
 inFileBtn.grid(row=2, column=8, sticky='WE', padx=5, pady=2)
+
 
 #inFileTxt.insert(INSERT, )
 
@@ -86,10 +95,10 @@ stepTwo.grid(row=3, columnspan=7, sticky='W', \
 inFileLb2 = Tkinter.Label(stepTwo, text="Escolher diretório de imagens:")
 inFileLb2.grid(row=4, column=0, sticky='WE', padx=5, pady=2)
 
-inFileTxt2 = Tkinter.Entry(stepTwo)
-inFileTxt2.grid(row=4, column=1, columnspan=7, sticky="WE", pady=3)
+inFileTxt2 = Tkinter.Text(stepTwo,width = 23, height=1)
+inFileTxt2.grid(row=4, column=1, columnspan=7, sticky="WE",pady=3)
 
-inFileBtn2 = Tkinter.Button(stepTwo, text="Buscar")
+inFileBtn2 = Tkinter.Button(stepTwo, text="Buscar", command = open_dir1)
 inFileBtn2.grid(row=4, column=8, sticky='W', padx=5, pady=2)
 
 
@@ -102,10 +111,10 @@ stepThree.grid(row=5, columnspan=7, sticky='W', \
 inFileLb3 = Tkinter.Label(stepThree, text="Escolher diretório de imagens:")
 inFileLb3.grid(row=4, column=0, sticky='WE', padx=5, pady=2)
 
-inFileTxt3 = Tkinter.Entry(stepThree)
-inFileTxt3.grid(row=4, column=1, columnspan=7, sticky="WE", pady=3)
+inFileTxt3 = Tkinter.Text(stepThree,width = 23, height=1)
+inFileTxt3.grid(row=4, column=1, columnspan=7, sticky="WE",pady=3)
 
-inFileBtn3 = Tkinter.Button(stepThree, text="Buscar")
+inFileBtn3 = Tkinter.Button(stepThree, text="Buscar", command = open_dir2)
 inFileBtn3.grid(row=4, column=8, sticky='W', padx=5, pady=2)
 
 # ------- Help section -------
@@ -124,7 +133,7 @@ scrollbar.grid(row=0, column=9, columnspan=2, rowspan=8, \
 
 scrollbar.config(command=helpText.yview)
 
-helpText.insert(INSERT, "\nEste e o programa de correção automatica de provas de múltipla escolha. Para efetuar as correções, preencha os campos  ao lado. \n\n -> Passo 1. \nDefinir sequência de respostas corretas do gabarito ou informar imagem do gabarito correto preenchido \n\n -> Passo 2. \nInformar diretório com imagens de respostas dos alunos. \n\n -> Passo 3. \nEscolher diretorio para salvar o resultado dos alunos")
+helpText.insert(INSERT, "\nEste e o programa de correção automatica de provas de múltipla escolha. Para efetuar as correções, preencha os campos  ao lado. \n\n -> Passo 1. \nDefinir sequência de respostas corretas do gabarito ou informar imagem do gabarito correto preenchido \n\n -> Passo 2. \nInformar diretório com imagens de respostas dos alunos. \n\n -> Passo 3. \nEscolher diretorio para salvar o resultado dos alunos\n")
 
 helpText.configure(yscrollcommand=scrollbar.set, state='disable')
 
