aae95023e7ffde8d1410f2523dd023d2860ad0a9
dinalivia <dinalivia@bct.ect.ufrn.br>
2019-06-26 18:21:19 -0700

    add img files

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,31 @@
-python orb.py "../img/pi_cam/testA1.png" "../img/im_filtered/filtered1.jpg"
+python orb.py "../img/pi_cam/testA1.png" "../img/im_filtered/filteredA1.jpg"
 
-python pre_processing.py "../img/im_filtered/filtered1.jpg" "../img/ROI/img_roi1.jpg"
+python pre_processing.py "../img/im_filtered/filteredA1.jpg" "../img/ROI/img_roiA1.jpg"
 
-python process_gabarito.py "../img/ROI/img_roi1.jpg" "../answers/answers1.csv"
+python process_gabarito.py "../img/ROI/img_roiA1.jpg" "../answers/answersA1.csv"
 
-python correction.py '../answers/answers1.csv' '../gabarito/answers.csv'
+python correction.py '../answers/answersA1.csv' '../gabarito/answers.csv'
+
+
+
+
+
+python orb.py "../img/pi_cam/testC1.png" "../img/im_filtered/filteredC1.jpg"
+
+python pre_processing.py "../img/im_filtered/filteredC1.jpg" "../img/ROI/img_roiC1.jpg"
+
+python process_gabarito.py "../img/ROI/img_roiC1.jpg" "../answers/answersC1.csv"
+
+python correction.py '../answers/answersC1.csv' '../gabarito/answers.csv'
+
+
+
+
+python orb.py "../img/pi_cam/testC2.png" "../img/im_filtered/filteredC2.jpg"
+
+python pre_processing.py "../img/im_filtered/filteredC2.jpg" "../img/ROI/img_roiC2.jpg"
+
+python process_gabarito.py "../img/ROI/img_roiC2.jpg" "../answers/answersC2.csv"
+
+python correction.py '../answers/answersC2.csv' '../gabarito/answers.csv'
 
